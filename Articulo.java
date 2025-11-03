c4afa3942423ab3aed46505f252cbbbef628704b
Jimena <mjimevm@gmail.com>
2025-11-03 11:36:28 -0600

    Modified class

--- Articulo.java
+++ Articulo.java
@@ -1,3 +1,33 @@
+import java.util.ArrayList;
+
 public class Articulo extends Contenido {
-    
+    private String autor;
+    private String fechaPublicacion;
+    private String horaPublicacion;
+    private String resumen;
+
+    public Articulo(String nombre, String descripcion, ArrayList<String> etiquetas, String categoria, String autor, String fechaPublicacion, String horaPublicacion, String resumen) {
+        super(nombre,  descripcion, etiquetas, categoria);
+        this.autor = autor;
+        this.fechaPublicacion = fechaPublicacion;
+        this.horaPublicacion = horaPublicacion;
+        this.resumen = resumen;
+    }
+    public String getAutor() {
+        return autor;
+    }
+    public String getFechaPublicacion() {
+        return fechaPublicacion;
+    }
+    public String getHoraPublicacion() {
+        return horaPublicacion;
+    }
+    public String getResumen() {
+        return resumen;
+    }
+    @Override 
+    public String toString() {
+        return super.toString() + ", Autor: " + autor + ", Fecha de Publicación: " + fechaPublicacion + ", Hora de Publicación: " + horaPublicacion + ", Resumen: " + resumen + "]";
+    }
+
 }
